cf80f0796ed2840aa9f13168407447b998214fec
Ushik Shrestha <ushik.shrestha@rmit.edu.vn>
2022-03-03 09:52:32 +0700

    Initial commit

---
+++ file1.py
@@ -0,0 +1,7 @@
+#temp_cel = float(input('Enter the temperature in centigrade: '))
+temp_fah = float(temp_cel * (9/5) +32)
+print(temp_fah)
+
+int 3 , 7 , 22
+
+float 3.4, 5.7, 5.99
---
+++ week1/tutorial1.py
@@ -0,0 +1,1 @@
+print('Hello, I am creating a file within subdirectory.')
